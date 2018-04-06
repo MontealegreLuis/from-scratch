21d949f7bd9dae2fd554b62c7474aa06b1ef389a
Luis Montealegre <montealegreluis@gmail.com>
2018-04-05 21:55:55 -0500

    chore: Add function to build a table of nodes

--- compressor/myzip.rb
+++ compressor/myzip.rb
@@ -1,5 +1,22 @@
 #!/usr/bin/env ruby
 
+def compress(original)
+  tree = build_tree(original)
+  table = build_table(tree)
+
+  original.bytes.each do |byte|
+    bits = look_up_byte(table, byte)
+  end
+end
+
+def look_up_byte(table, byte)
+  table.each do |row|
+    if row.byte == byte
+      return row.bits
+    end
+  end
+end
+
 def build_tree(original)
   bytes = original.bytes
   unique_bytes = bytes.uniq
@@ -12,10 +29,19 @@ def build_tree(original)
     node2 = nodes.delete(nodes.min_by(&:count))
     nodes << Node.new(node1, node2, node1.count + node2.count)
   end
-  nodes
+  nodes.fetch(0)
+end
+
+def build_table(node, path = [])
+  if node.is_a? Node
+    build_table(node.left, path + [0]) + build_table(node.right, path + [1])
+  else
+    [TableRow.new(node.byte, path)]
+  end
 end
 
 Node = Struct.new(:left, :right, :count)
 Leaf = Struct.new(:byte, :count)
+TableRow = Struct.new(:byte, :bits)
 
-p build_tree('abbcccc')
+p compress('abbcccc')
