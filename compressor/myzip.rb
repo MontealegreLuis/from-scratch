96145ae730d608a6c90ef475d70d13bfaef305bb
Luis Montealegre <montealegreluis@gmail.com>
2018-04-06 21:25:27 -0500

    feature: Complete the compressor/decompressor

--- compressor/myzip.rb
+++ compressor/myzip.rb
@@ -1,11 +1,59 @@
 #!/usr/bin/env ruby
 
+require './binpacker.rb'
+
 def compress(original)
   tree = build_tree(original)
   table = build_table(tree)
+  packer = BinPacker.new
+
+  packer.int32(original.length)
+  pack_table(table, packer)
 
   original.bytes.each do |byte|
     bits = look_up_byte(table, byte)
+    packer.bits(bits)
+  end
+
+  packer.pack
+end
+
+def decompress(compressed)
+  unpacker = BinUnpacker.new(compressed)
+
+  data_length = unpacker.int32
+  table = unpack_table(unpacker)
+
+  data_length.times.map do
+    look_up_bits(table, unpacker)
+  end.map(&:chr).join
+end
+
+def unpack_table(unpacker)
+  table_length = unpacker.int8 + 1
+  table_length.times.map do
+    byte = unpacker.int8
+    bit_count = unpacker.int8
+    bits = unpacker.bits(bit_count)
+    TableRow.new(byte, bits)
+  end
+end
+
+def look_up_bits(table, unpacker)
+  table.each do |row|
+    if row.bits == unpacker.peek(row.bits.length)
+      unpacker.bits(row.bits.length)
+      return row.byte
+    end
+  end
+end
+
+def pack_table(table, packer)
+  packer.int8(table.length - 1)
+  table.each do |row|
+    packer.int8(row.byte)
+    packer.int8(row.bits.length)
+    packer.bits(row.bits)
   end
 end
 
@@ -44,4 +92,8 @@ Node = Struct.new(:left, :right, :count)
 Leaf = Struct.new(:byte, :count)
 TableRow = Struct.new(:byte, :bits)
 
-p compress('abbcccc')
+if ARGV.fetch(0) == "compress"
+  $stdout.write(compress($stdin.read))
+else
+  $stdout.write(decompress($stdin.read))
+end
